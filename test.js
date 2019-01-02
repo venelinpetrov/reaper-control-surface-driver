f88c6a3805433402e5e0faee61c0070729d0dc41
venelinpetrov <vpetrov33tu@gmail.com>
2019-01-02 19:55:01 +0200

    Remove redundant code Remove UI-related code

--- test.js
+++ test.js
@@ -28,24 +28,10 @@ let
 
 let sel_trk_idx = null;
 
-function setTextForObject(obj, text) { // thx cfillion
-    if (obj.lastChild) obj.lastChild.nodeValue = text;
-    else obj.appendChild(document.createTextNode(text));
-}
-
-function mouseDownEventHandler(msg) {
-    return function (e) {
-        if (typeof e == 'undefined') e = event;
-        if (e.preventDefault) e.preventDefault();
-        wwr_req(msg);
-        return false;
-    }
-}
-
 function wwr_onreply(results) {
-    var ar = results.split("\n");
-    for (var x = 0; x < ar.length; x++) {
-        var tok = ar[x].split("\t");
+    const ar = results.split("\n");
+    for (let x = 0; x < ar.length; x++) {
+        const tok = ar[x].split("\t");
         if (tok.length > 0) switch (tok[0]) {
             case "TRANSPORT":
                 if (tok.length > 4) {
@@ -55,34 +41,12 @@ function wwr_onreply(results) {
                             g_outputs_by_name["Maschine Mikro MK2 Out"].send([176, PLAY_CC, (last_transport_state & 1) > 0 ? 127 : 0]);
                             g_outputs_by_name["Maschine Mikro MK2 Out"].send([176, REC_CC, (last_transport_state & 4) > 0 ? 127 : 0]);
                         }
-                        document.getElementById("play").style.background = (last_transport_state & 1) ? "#0f0" : "#fff";
-                        document.getElementById("rec").style.background = (last_transport_state & 4) ? "#f33" : "#fff";
-                    }
-                    var obj = document.getElementById("status");
-                    if (obj) {
-                        var tmp = "";
-                        switch (parseInt(last_transport_state)) {
-                            case 0: tmp += "stopped: "; break;
-                            case 1: tmp += "playing: "; break;
-                            case 2: tmp += "paused: "; break;
-                            case 5: tmp += "recording: "; break;
-                            case 6: tmp += "recpaused: "; break;
-                        }
-
-                        tmp += (last_time_str = tok[4]);
-                        setTextForObject(obj, tmp);
                     }
                 }
                 break;
             case "NTRACK":
-                if (tok.length > 1 && tok[1] >= 0) {
-                    var host = document.getElementById("tracks");
-                    if (host) {
-                        last_track_cnt = parseInt(tok[1]) + 1;
-                        var l = host.rows.length;
-                        while (l > last_track_cnt) host.deleteRow(--l);
-                    }
-                }
+                if (tok.length > 1 && tok[1] >= 0)
+                    last_track_cnt = parseInt(tok[1]) + 1;
                 break;
             case "TRACK":
                 if (tok.length > 5) {
@@ -97,91 +61,14 @@ function wwr_onreply(results) {
     }
 }
 
-
 wwr_req_recur("TRANSPORT", 10);
 wwr_req_recur("GET/TRACK/0/SEND/0;GET/TRACK/0/SEND/1;NTRACK;TRACK;GET/40364", 1000);
 wwr_start();
 
-function updateOrientation() {
-    var a = document.getElementById("viewport-meta");
-    if (a) a.setAttribute("content", "width=320, user-scalable = no, minimum-scale = 1.0, maximum-scale = 1.0, initial-scale = 1.0");
-}
-
-function prompt_abort() {
-    if (!(last_transport_state & 4)) {
-        wwr_req(1016);
-    } else {
-        if (confirm("abort recording? contents will be lost!")) wwr_req(40668);
-    }
-}
-
-function prompt_seek() {
-    if (!(last_transport_state & 4)) {
-        var seekto = prompt("Seek to position:", last_time_str);
-        if (seekto != null) {
-            wwr_req("SET/POS_STR/" + encodeURIComponent(seekto));
-        }
-    }
-}
-
-function prompt_clone() {
-    if (g_inspect_tridx > 0) {
-        var title = last_titles[g_inspect_tridx];
-        var newtitle = prompt("Clone track " + title.replace(/[.] .*/, "") + " without media to new track:",
-            title.replace(/^[0-9]*?[.] /, ""));
-        if (newtitle != null) {
-            wwr_req("SET/UNDO_BEGIN;40297;SET/TRACK/" + g_inspect_tridx + "/SEL/1;40062;40421;40006;" +
-                "SET/TRACK/" + (g_inspect_tridx + 1) + "/P_NAME/" + encodeURIComponent(newtitle) +
-                ";GET/TRACK/" + g_inspect_tridx + ";SET/UNDO_END/Clone%20track%20without%20media");
-
-            inspect_track_handler(g_inspect_tridx + 1)();
-        }
-    }
-    return false;
-}
-
-function inspect_track_handler(tidx) {
-    return function (e) {
-        if (typeof e == 'undefined') e = event;
-        if (e.preventDefault) e.preventDefault();
-        g_inspect_tridx = parseInt(tidx);
-        document.getElementById("trackinspect").style.display = tidx >= 0 ? "inline" : "none";
-        if (tidx >= 0) {
-            document.ontouchmove = function (e) { e.preventDefault(); }
-
-            last_titles[tidx] = null;
-            last_vols[tidx] = null;
-            last_flags[tidx] = null;
-            wwr_req("TRACK/" + tidx);
-            if (tidx == 0) {
-                var c = document.getElementById("trackinspect_arm");
-                c.innerHTML = "main<br>out";
-                c.style.background = last_out_states[0] ? "#88f" : "#fff";
-
-                c = document.getElementById("trackinspect_mon");
-                c.innerHTML = "aux<br>out";
-                c.style.background = last_out_states[1] ? "#88f" : "#fff";
-
-                c = document.getElementById("trackinspect_clone");
-                c.innerHTML = "metronome";
-                c.style.background = last_metronome ? "#8f8" : "#777";
-            } else {
-                document.getElementById("trackinspect_arm").innerHTML = "arm";
-                document.getElementById("trackinspect_mon").innerHTML = "mon";
-                document.getElementById("trackinspect_clone").innerHTML = "clone<br>without<br>media";
-            }
-        } else {
-            setTextForObject(document.getElementById("trackinspect_title"), "");
-            setTextForObject(document.getElementById("trackinspect_volh"), "");
-            document.ontouchmove = function (e) { return true; }
-        }
-        return false;
-    };
-}
 g_outputs = null;
 g_outputs_by_name = {};
+
 function init() {
-    updateOrientation();
     //Request midi accsess
     navigator.requestMIDIAccess().then(function (midi_access) {
         console.log("MIDI ready!");
@@ -267,88 +154,4 @@ function init() {
         // will refresh the state accordingly.
         wwr_req('TRACK');
     }
-
-    document.getElementById("trackinspect_clone").onmousedown = function (e) {
-        if (g_inspect_tridx > 0) return prompt_clone();
-        return mouseDownEventHandler("40364;GET/40364")(e);
-    };
-    document.getElementById("trackinspect_arm").onmousedown = function (e) {
-        if (g_inspect_tridx == 0)
-            return mouseDownEventHandler("SET/TRACK/0/SEND/0/MUTE/-1;GET/TRACK/0/SEND/0")(e);
-        return mouseDownEventHandler("SET/TRACK/" + g_inspect_tridx + "/RECARM/-1;TRACK/" + g_inspect_tridx)(e);
-    };
-    document.getElementById("trackinspect_mon").onmousedown = function (e) {
-        if (g_inspect_tridx == 0)
-            return mouseDownEventHandler("SET/TRACK/0/SEND/1/MUTE/-1;GET/TRACK/0/SEND/1")(e);
-        return mouseDownEventHandler("SET/TRACK/" + g_inspect_tridx + "/RECMON/-1;TRACK/" + g_inspect_tridx)(e);
-    };
-    document.getElementById("trackinspect_mute").onmousedown = function (e) {
-        return mouseDownEventHandler("SET/TRACK/" + g_inspect_tridx + "/MUTE/-1;TRACK/" + g_inspect_tridx)(e);
-    };
-    document.getElementById("trackinspect_solo").onmousedown = function (e) {
-        mouseDownEventHandler("SET/TRACK/" + g_inspect_tridx + "/SOLO/-1;TRACK/" + g_inspect_tridx)(e);
-    };
-}
-
-function volfader(e, istouch) {
-    var fader = document.getElementById("trackinspect_volf");
-    var handle = document.getElementById("trackinspect_volh");
-    if (typeof e == 'undefined') e = event;
-    if (e.preventDefault) e.preventDefault();
-    e.returnValue = false;
-
-    var sh = fader.offsetHeight - handle.offsetHeight;
-    if (sh < 10) sh = 10;
-    var starty = e.pageY || e.clientY || e.touches[0].pageY;
-
-    var movee = function (e) {
-        if (typeof e == 'undefined') e = event;
-        if (e.preventDefault) e.preventDefault();
-        e.returnValue = false;
-        var thisy = e.pageY || e.clientY || e.touches[0].pageY;
-        var dy = thisy - starty;
-        starty = thisy;
-
-        dy = - 100 * dy / sh;
-        if (dy >= 0) dy = "+" + dy;
-        wwr_req("SET/TRACK/" + g_inspect_tridx + "/VOL/" + dy + ";TRACK/" + g_inspect_tridx)
-        return false;
-    };
-
-    if (istouch) {
-        var end = function (e) {
-            document.removeEventListener('touchmove', movee, true);
-            document.removeEventListener('touchend', end, true);
-            return false;
-        };
-        document.addEventListener('touchmove', movee, true);
-        document.addEventListener('touchend', end, true);
-    } else {
-        document.onmousemove = movee;
-        document.onmouseup = function (e) {
-            document.onmousemove = null;
-            document.onmouseup = null;
-            return false;
-        }
-    }
-    return false;
-};
-
-function rename_track(e) {
-    if (g_inspect_tridx > 0) {
-        var title = last_titles[g_inspect_tridx];
-        var newtitle = prompt("Rename track " + title.replace(/[.] .*/, "") + ":",
-            title.replace(/^[0-9]*?[.] /, ""));
-        if (newtitle != null) {
-            wwr_req("SET/TRACK/" + g_inspect_tridx + "/P_NAME/" + encodeURIComponent(newtitle) + ";GET/TRACK/" + g_inspect_tridx + ";SET/UNDO/Rename%20Track");
-        }
-    }
-    return false;
-}
-
-function on_record_button(e) {
-    var x;
-    for (x = 1; x < last_track_cnt && 0 == (last_flags[x] & 64); x++);
-    if (x < last_track_cnt || confirm("no tracks are armed, start record?")) wwr_req(1013);
-    return false;
 }
